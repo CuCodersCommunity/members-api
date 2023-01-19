c37c2bf1b7def6c1e4a10ceff40c6e4756d59418
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-01-19 15:58:04 -0500

    refactoring api to member and member list endpoints

--- src/pages/member/[id].js
+++ src/pages/member/[id].js
@@ -1,7 +1,3 @@
-import { APIRoute } from "astro";
-import { Octokit } from "octokit";
-import memberLists from "../../../data/members.json";
-
 var users_data = {};
 
 const allPosts = await import.meta.glob("../../../data/members/*.json");
@@ -11,13 +7,6 @@ for (const path in allPosts) {
     const asd = mod.default;
     console.log(asd.user);
   });
-}
-
-for (const member of memberLists) {
-  const octokit = new Octokit({ auth: import.meta.env.SECRET_GITHUB_TOKEN });
-  const response = await octokit.request("GET /users/" + member, { username: member });
-  const user_data = await response.data;
-  users_data[member] = user_data;
 }
 
 export async function get({ params, request }) {
--- src/pages/members-list.js
+++ src/pages/members-list.js
@@ -1,15 +1,14 @@
 const data = import.meta.glob("../data/members/*.json");
-import { expandMemberAPI } from "../helpers/extendMemberAPI";
-var members = {};
+var memberList = [];
 
 for (const path in data) {
   await data[path]().then((mod) => {
-    members[mod.username] = expandMemberAPI(mod);
+    memberList.push(mod.username);
   });
 }
 
 export async function get({ params, request }) {
   return {
-    body: JSON.stringify(members),
+    body: JSON.stringify(memberList),
   };
 }
