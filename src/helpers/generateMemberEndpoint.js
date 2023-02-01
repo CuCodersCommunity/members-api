6c664ee2e95ce66231a76605194b63912f189b2c
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-02-01 12:01:27 -0500

    creating profile data if profile is not created

--- src/helpers/generateMemberEndpoint.js
+++ src/helpers/generateMemberEndpoint.js
@@ -1,4 +1,5 @@
 import { getRemoteFileData } from "./getRemoteFile";
+import { backend_url } from "../config.json";
 
 async function generateMemberEndpoint(githubMemberData) {
   var profileData = {
@@ -32,7 +33,33 @@ async function generateMemberEndpoint(githubMemberData) {
     profileData.categories = data.categories;
     profileData.telegram_username = data.telegram_username;
     profileData.linkedin_username = data.linkedin_username;
+  } else {
+    profileData = await getGitHubUserData(githubMemberData.login);
   }
+
+  return profileData;
+}
+
+async function getGitHubUserData(profileData) {
+  const url = backend_url + "api/members/get-member?username=" + profileData.username;
+
+  const respose = await fetch(url, { method: "GET" });
+  const data = await respose.json();
+
+  if (!data) {
+    return profileData;
+  }
+
+  if (data.name) {
+    profileData.name = data.name;
+  }
+  profileData.company = data.company;
+  profileData.web = data.blog;
+  profileData.location = data.location;
+  profileData.email = data.email;
+  profileData.hireable = data.hireable;
+  profileData.bio = data.bio;
+  profileData.twitter_username = data.twitter_username;
 
   return profileData;
 }
