050b0faa7a08b5986a55e5278bc8254e83728c87
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2023-04-06 15:48:41 -0400

    removing get github users external data

--- src/helpers/generateMemberEndpoint.js
+++ src/helpers/generateMemberEndpoint.js
@@ -11,7 +11,7 @@ async function generateMemberEndpoint(githubMemberData) {
     avatar_url_small: "https://avatars.githubusercontent.com/u/" + githubMemberData.id + "?s=64&v=4",
     avatar_url_medium: "https://avatars.githubusercontent.com/u/" + githubMemberData.id + "?s=128&v=4",
     github_url: "https://github.com/" + githubMemberData.login,
-    categories: []
+    categories: [],
   };
   let data = await getRemoteFileData(
     `https://raw.githubusercontent.com/${githubMemberData.login}/${githubMemberData.login}/main/cucoders_data/profile-data.json`,
@@ -33,34 +33,7 @@ async function generateMemberEndpoint(githubMemberData) {
     profileData.categories = data.categories;
     profileData.telegram_username = data.telegram_username;
     profileData.linkedin_username = data.linkedin_username;
-  } else {
-    profileData = await getGitHubUserData(profileData);
   }
-
-  return profileData;
-}
-
-async function getGitHubUserData(profileData) {
-  const url = backend_url + "api/members/get-member?username=" + profileData.username;
-
-  const respose = await fetch(url, { method: "GET" });
-  const data = await respose.json();
-
-  if (!data) {
-    return profileData;
-  }
-
-  if (data.name) {
-    profileData.name = data.name;
-  }
-  profileData.company = data.company;
-  profileData.web = data.blog;
-  profileData.location = data.location;
-  profileData.email = data.email;
-  profileData.hireable = data.hireable;
-  profileData.bio = data.bio;
-  profileData.twitter_username = data.twitter_username;
-
   return profileData;
 }
 
